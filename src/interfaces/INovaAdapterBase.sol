806fb0a1c3f9550dffc33752f61126d61719ac03
lakonema2000 <156841739+lakonema2000@users.noreply.github.com>
2024-05-21 13:12:15 +0200

    Add NovaVault and interfaces (#13)

---
+++ src/interfaces/INovaAdapterBase.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {ERC20} from "@solmate/tokens/ERC20.sol";
+
+interface INovaAdapterBase {
+    function asset() external view returns (ERC20);
+    function deposit(uint256 assets) external returns (bool , uint256);
+    function withdraw(uint256 shares) external returns (bool, uint256);
+}
