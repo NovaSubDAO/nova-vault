aaeda7b073dadd79c0ccad02c1e9f36f255a1ee6
Imad-Allal <imad.allal@outlook.com>
2024-06-01 17:05:43 +0100

    Adding the function to switch between adapters

--- src/NovaAdapterVeloB.sol
+++ src/NovaAdapterVeloB.sol
@@ -1,26 +1,23 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.17;
+pragma solidity ^0.8.13;
 
-import {IVelodromePool} from "./interfaces/IVelodromePool.sol";
-import {ERC20} from "@solmate/tokens/ERC20.sol";
+import {IVelodromePoolB} from "./interfaces/IVelodromePoolB.sol";
+import {IERC20} from "./interfaces/IERC20.sol";
 import {NovaAdapterBase} from "./NovaAdapterBase.sol";
-import {SafeTransferLib} from "@solmate/utils/SafeTransferLib.sol";
 
-contract NovaAdapterVelo is NovaAdapterBase {
-    using SafeTransferLib for ERC20;
-
+contract NovaAdapterVeloB is NovaAdapterBase {
     bool private isStableFirst;
     address immutable veloToken0;
     address immutable veloToken1;
 
-    IVelodromePool public immutable veloPool;
+    IVelodromePoolB public veloPool;
 
     constructor(
         address _asset,
         address _sDAI,
         address _pool
     ) NovaAdapterBase(_asset, _sDAI) {
-        veloPool = IVelodromePool(_pool);
+        veloPool = IVelodromePoolB(_pool);
         veloToken0 = veloPool.token0();
         veloToken1 = veloPool.token1();
 
@@ -41,30 +38,21 @@ contract NovaAdapterVelo is NovaAdapterBase {
         require(msg.sender == address(veloPool), "Caller is not VelodromePool");
 
         if (amount0Delta > 0) {
-            ERC20(veloToken0).safeTransfer(msg.sender, uint256(amount0Delta));
+            IERC20(veloToken0).transfer(msg.sender, uint256(amount0Delta));
         }
         if (amount1Delta > 0) {
-            ERC20(veloToken1).safeTransfer(msg.sender, uint256(amount1Delta));
+            IERC20(veloToken1).transfer(msg.sender, uint256(amount1Delta));
         }
     }
 
     function _swap(
-        int256 amount,
-        bool fromStableTosDai
-    ) internal override returns (int256, int256) {
-        (uint160 sqrtPriceX96, , , , , ) = veloPool.slot0();
-        uint160 num = fromStableTosDai ? 95 : 105;
-        int256 sign = isStableFirst ? int256(1) : int256(-1);
+        uint256 amount0Out,
+        uint256 amount1Out,
+        address to
+    ) internal override returns (int256) {
+        veloPool.swap(amount0Out, amount1Out, to, "");
 
-        (int256 amount0, int256 amount1) = veloPool.swap(
-            address(this),
-            fromStableTosDai,
-            sign * amount,
-            (num * sqrtPriceX96) / 100,
-            ""
-        );
-
-        return (amount0, amount1);
+        return int256(veloPool.getAmountOut(amount0Out, veloToken0));
     }
 
     function getAsset() external view returns (address) {
